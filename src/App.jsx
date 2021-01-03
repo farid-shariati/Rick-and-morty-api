8d9abca1c10c014dac62bc1b8c4e64c7e3db4f9a
unknown <faridshariati989@gmail.com>
2021-01-03 19:50:41 +0330

    Finish project I finish this

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ function App() {
             <Route path="/character/:id" component={CharacterDetail}/>
             <Route path="/location/:id" component={LocationDetail}/>
             <Route path="/episode/:id" component={EpisodeDetail}/>
+            <Route path="/episode/:id" component={EpisodeDetail}/>
           </div>
         </Router>
       </Provider>
